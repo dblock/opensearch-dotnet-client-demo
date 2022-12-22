5ddb508b3737482e61c2ccf92d6cbd33e62b7a4b
dblock <dblock@amazon.com>
2022-12-22 12:05:18 -0500

    OpenSearch DotNet (.NET) SigV4 Demo.
    Signed-off-by: dblock <dblock@amazon.com>

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿/* SPDX-License-Identifier: Apache-2.0
+*
+* The OpenSearch Contributors require contributions made to
+* this file be licensed under the Apache-2.0 license or a
+* compatible open source license.
+*/
+
+using Newtonsoft.Json;
+using OpenSearch.Client;
+using OpenSearch.Net.Auth.AwsSigV4;
+
+namespace Application
+{
+    class Program
+    {
+        protected class Person
+        {
+            public int Id { get; set; }
+            public string? FirstName { get; set; }
+        };
+
+        static void Main(string[] args)
+        {
+            var endpoint = new Uri(Environment.GetEnvironmentVariable("OPENSEARCH_ENDPOINT") ?? throw new ArgumentNullException("Missing OPENSEARCH_ENDPOINT."));
+            var region = Amazon.RegionEndpoint.GetBySystemName(Environment.GetEnvironmentVariable("OPENSEARCH_REGION") ?? "us-west-2");
+            var connection = new AwsSigV4HttpConnection(region);
+            var config = new ConnectionSettings(endpoint, connection);
+            var client = new OpenSearchClient(config);
+
+            Console.WriteLine($"{client.RootNodeInfo().Version.Distribution}: {client.RootNodeInfo().Version.Number}");
+
+            var index_name = "sample-index";
+            var index = client.Indices.Create(index_name);
+
+            try
+            {
+                // index a document
+                var document = new Person()
+                {
+                    Id = 1,
+                    FirstName = "Bruce"
+                };
+
+                client.Index(document, idx => idx.Index(index_name));
+
+                // wait for the document to index
+                Thread.Sleep(3 * 1000);
+
+                var results = client.Search<Person>(s => s
+                    .Index(index_name)
+                    .Query(q => q.Match(
+                        mq => mq.Field(f => f.FirstName)
+                            .Query("bruce")))
+                    );
+
+                foreach (var result in results.Hits)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(result));
+                }
+
+                client.Delete<Person>(document, idx => idx.Index(index_name));
+            }
+            finally
+            {
+                client.Indices.Delete(index_name);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+# OpenSearch DotNet (.NET) Client Demo
+
+Makes requests to Amazon OpenSearch using the [OpenSearch DotNet Client](https://github.com/opensearch-project/opensearch-net).
+
+### Install Prerequisites
+
+#### DotNet Framework
+
+Install [DotNet](https://learn.microsoft.com/en-us/dotnet/core/install). YMMV.
+
+```
+$ dotnet --version
+7.0.100
+```
+
+## Running
+
+Create an OpenSearch domain in (AWS) which support IAM based AuthN/AuthZ and run the demo.
+
+```
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_SESSION_TOKEN=
+
+export OPENSEARCH_ENDPOINT=https://....us-west-2.es.amazonaws.com
+export OPENSEARCH_REGION=us-west-2
+
+dotnet run
+```
+
+The [code](Program.cs) will connect to OpenSearch, display its version, create an index, add a document, search for it, output the search result, then cleanup.
+
+```
+opensearch: 2.3.0
+
+{"Explanation":null,"Fields":null,"Highlight":{},"Id":"1","Index":"sample-index","InnerHits":{},"MatchedQueries":[],"Nested":null,"PrimaryTerm":null,"Routing":null,"Score":0.2876821,"SequenceNumber":null,"Sorts":[],"Source":{"Id":1,"FirstName":"Bruce"},"Type":null,"Version":0}
+```
+
+## License 
+
+This project is licensed under the [Apache v2.0 License](LICENSE.txt).
+
+## Copyright
+
+Copyright OpenSearch Contributors. See [NOTICE](NOTICE.txt) for details.
